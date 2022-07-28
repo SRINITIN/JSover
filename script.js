5ac3c939ce8c6c34cd4270267d3d96bb5bd61d2e
unknown <nitin31sri@gmail.com>
2022-07-28 22:41:33 +0530

    flow

---
+++ script.js
@@ -0,0 +1,57 @@
+var x = 3118;
+
+function getName(){
+    console.log("NITIN RITIKA");
+}
+
+// getName();
+// console.log(x);
+
+
+// getName();
+// console.log(x);
+
+// var x = 3118;
+
+// function getName(){
+//     console.log("NITIN RITIKA");
+// }
+
+
+// var x = 3118;
+
+// function getName(){
+//     console.log("NITIN RITIKA");
+// }
+
+// console.log(getName);
+
+
+// console.log(getName);
+
+// function getName(){
+//     console.log("NITIN RITIKA");
+// }
+
+
+// getName();
+// console.log(x);
+// console.log(getName);
+// var x = 3118;
+
+// function getName(){
+//     console.log("NITIN RITIKA");
+// }
+
+
+// var x = 3118;
+
+// function getName(){
+//     console.log("NITIN RITIKA");
+// }
+
+// getName();
+// console.log(x);
+// console.log(getName);
+
+
---
+++ squarefn.js
@@ -0,0 +1,7 @@
+function square(x){
+    var y = x * x;
+    return y;
+}
+var a = 2;
+var b = square(a);
+var c = square(9);
